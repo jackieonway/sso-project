cbaccf62875f60f089acac5c87fb8625bc58e01d
jackieonway <1254825871@qq.com>
2020-04-25 22:55:25 +0800

    feat: add AuthorizationServerConfig

--- sso-project/sso-server/src/main/java/com/github/jackieonway/config/AuthorizationServerConfig.java
+++ sso-project/sso-server/src/main/java/com/github/jackieonway/config/AuthorizationServerConfig.java
@@ -4,12 +4,24 @@
  */
 package com.github.jackieonway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 /**
  * @author Jackie
@@ -19,19 +31,64 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public TokenStore tokenStore(){
+        return new InMemoryTokenStore();
+    }
+
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private AuthorizationCodeServices authorizationCodeServices;
+
+    //token 令牌管理服务
+    @Bean
+    public AuthorizationServerTokenServices tokenServices(){
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setClientDetailsService(clientDetailsService);
+        tokenServices.setReuseRefreshToken(true);
+        tokenServices.setTokenStore(tokenStore());
+        tokenServices.setRefreshTokenValiditySeconds(36000);
+        tokenServices.setAccessTokenValiditySeconds(3600);
+        return tokenServices;
+    }
+
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         super.configure(security);
     }
 
+    // 客户端配置
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        super.configure(clients);
+//        super.configure(clients);
+        clients.inMemory()
+                .withClient("c1")
+                .secret(passwordEncoder().encode("123456"))
+                .resourceIds("res1")
+                .authorizedGrantTypes("authorization_code","refresh_token","password") //模式
+                .scopes("all") // 授权范围
+                .autoApprove(false) // false 表示会跳转到授权页面
+                .redirectUris("http://www.baidu.com"); //重定向地址
     }
 
+    // 令牌访问端点
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        super.configure(endpoints);
+//        super.configure(endpoints);
+        endpoints.authenticationManager(authenticationManager) //password 模式需要配置的
+                .authorizationCodeServices(authorizationCodeServices) // authorization_code授权码模式需要配置的
+                .tokenServices(tokenServices()) //令牌管理服务
+                .allowedTokenEndpointRequestMethods(HttpMethod.GET,HttpMethod.POST); //允许GET、POST请求
     }
 }
