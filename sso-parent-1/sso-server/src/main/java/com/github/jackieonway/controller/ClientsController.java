4e784213d37d86a0d30461e8194e98531232aff6
jackieonway <1254825871@qq.com>
2020-04-27 16:55:36 +0800

    feat: add auto approve
    add auto approve

--- sso-parent-1/sso-server/src/main/java/com/github/jackieonway/controller/ClientsController.java
+++ sso-parent-1/sso-server/src/main/java/com/github/jackieonway/controller/ClientsController.java
@@ -4,6 +4,7 @@ import com.github.jackieonway.config.AuthorityPropertyEditor;
 import com.github.jackieonway.config.SplitCollectionEditor;
 import com.github.jackieonway.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.provider.ClientDetails;
@@ -11,11 +12,12 @@ import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Controller
@@ -23,6 +25,9 @@ import java.util.Set;
 public class ClientsController {
     @Autowired
     private JdbcClientDetailsService jdbcClientDetailsService;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     @InitBinder
     public void initBinder(WebDataBinder binder){
 
@@ -53,27 +58,49 @@ public class ClientsController {
     public String editClient(
             @ModelAttribute BaseClientDetails clientDetails,
             @RequestParam(value = "newClient", required = false) String newClient) {
-        if (newClient == null) {
-            String pwdEncode = Utils.passwordEncoder(clientDetails.getClientSecret());
-            clientDetails.setClientSecret(pwdEncode);
+        if (Objects.isNull(newClient)) {
+            if (!StringUtils.isEmpty(clientDetails.getClientSecret())){
+                String pwdEncode = Utils.passwordEncoder(clientDetails.getClientSecret());
+                jdbcClientDetailsService.updateClientSecret(clientDetails.getClientId(),pwdEncode);
+            }
         } else {
-            Set<String> autoApproval = new HashSet<>();
-            autoApproval.add("false");
-            clientDetails.setAutoApproveScopes(autoApproval);
+            if (StringUtils.isEmpty(clientDetails.getClientSecret())){
+               throw new RuntimeException("资源客户端密钥不能为空");
+            }
             String pwdEncode = Utils.passwordEncoder(clientDetails.getClientSecret());
             clientDetails.setClientSecret(pwdEncode);
             jdbcClientDetailsService.addClientDetails(clientDetails);
+            updateAutoApproval("false", clientDetails.getClientId());
+            return "redirect:/";
         }
-
-        if (!clientDetails.getClientSecret().isEmpty()) {
-            jdbcClientDetailsService.updateClientDetails(clientDetails);
-        }
+        jdbcClientDetailsService.updateClientDetails(clientDetails);
+        updateAutoApproval("false", clientDetails.getClientId());
         return "redirect:/";
     }
 
+    /**
+     * 更新自动授权字段
+     * @param autoapprove 自动授权
+     * @param clientId 客户端id
+     * @author  Jackie
+     * @date  2020/4/27 16:01
+     * @since 1.0
+     * @see ClientsController
+     */
+    private void updateAutoApproval(String autoapprove , String clientId) {
+        String sql = "UPDATE oauth_client_details SET autoapprove = '" + autoapprove + "'  WHERE client_id = '"+ clientId  +"';";
+        jdbcTemplate.execute(sql);
+    }
+
     @RequestMapping(value="{client.clientId}/delete",method = RequestMethod.GET)
-    public String deleteClient(@ModelAttribute BaseClientDetails clientDetails,@PathVariable("client.clientId") String id){
+    public String deleteClient(@PathVariable("client.clientId") String id){
         jdbcClientDetailsService.removeClientDetails(jdbcClientDetailsService.loadClientByClientId(id).getClientId());
+        return "redirect:/";
+    }
+
+    @RequestMapping(value="{client.clientId}/autoApprove",method = RequestMethod.GET)
+    public String cancelAutoApprove(@PathVariable("client.clientId") String clientId,String autoApprove){
+        updateAutoApproval(autoApprove, clientId);
         return "redirect:/";
     }
 }
